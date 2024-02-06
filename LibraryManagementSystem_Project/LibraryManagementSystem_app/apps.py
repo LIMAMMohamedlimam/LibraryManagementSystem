d210ffb52a8e3a3ddfee7f92f0c5d374e10f0b1d
LIMAMMohmedlimam <medlimamuia@gmail.com>
2024-02-06 19:09:38 +0100

    first commit : Setting up the project folders and dependencies.

---
+++ LibraryManagementSystem_Project/LibraryManagementSystem_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibrarymanagementsystemAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LibraryManagementSystem_app'
